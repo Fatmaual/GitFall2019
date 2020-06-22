4034cc61a5df3badd974ef06f5d4c94e1886740d
Fatmaual <58540374+Fatmaual@users.noreply.github.com>
2020-06-22 01:19:03 -0400

    Update Colors.java

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -4,7 +4,9 @@ public class Colors {
     public static void main(String[] args) {
         String [] colors={"red","blue","green","yellow"};
         String newBranch="Olivia Glen";
+        String secondName="Sari";
         System.out.println("newBranch = " + newBranch);
+        
 
 
     }
